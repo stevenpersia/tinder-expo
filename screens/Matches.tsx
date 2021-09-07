40e3ea2b7354f086f69b93649ab4ba1d6dc020a0
Drew Lee <andrew.lee.7451@gmail.com>
2021-09-07 16:00:09 -0400

    Changed Backgound Images for the screen

--- screens/Matches.tsx
+++ screens/Matches.tsx
@@ -13,16 +13,20 @@ import {
 } from "react-native";
 import { CardItem, Icon, ProfileItem } from "../components";
 import DEMO from "../assets/data/demo";
-import styles, { BLACK, DARK_GRAY, SECONDARY_COLOR, WHITE } from "../assets/styles";
+import styles, {
+  BLACK,
+  DARK_GRAY,
+  SECONDARY_COLOR,
+  WHITE,
+} from "../assets/styles";
 import IMAGE_01 from "../assets/images/nabil.jpg";
-
 
 const Matches = () => {
   const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <ImageBackground
-      source={require("../assets/images/bg.png")}
+      source={require("../assets/images/bg2.jpg")}
       style={styles.bg}
     >
       <View style={styles.containerMatches}>
@@ -38,35 +42,39 @@ const Matches = () => {
           data={DEMO}
           keyExtractor={(item, index) => index.toString()}
           renderItem={({ item }) => (
-            <TouchableOpacity
-            onPress={() => setModalVisible(true)}>
+            <TouchableOpacity onPress={() => setModalVisible(true)}>
               <CardItem
                 image={item.image}
                 name={item.name}
                 isOnline={item.isOnline}
                 hasVariant
               />
-                  <Modal
-                    animationType="slide"
-                    transparent={true}
-                    visible={modalVisible}
-                    onRequestClose={() => {
-                        Alert.alert("Modal has been closed.");
-                        setModalVisible(!modalVisible);
-                    }}
+              <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                  Alert.alert("Modal has been closed.");
+                  setModalVisible(!modalVisible);
+                }}
+              >
+                <View
+                  style={{
+                    alignSelf: "center",
+                    justifyContent: "center",
+                    backgroundColor: "white",
+                  }}
+                >
+                  <ImageBackground
+                    source={require("../assets/images/bg.png")}
+                    style={styles.bg}
                   >
-                      <View style={{ alignSelf: 'center', justifyContent: 'center', backgroundColor: "white" }}>
-                        <ImageBackground
-                          source={require("../assets/images/bg.png")}
-                          style={styles.bg}
-                        >
-                          <ScrollView style={styles.containerProfile}>
-                            <ImageBackground source={IMAGE_01} style={styles.photo}>
-                              <View style={styles.top}>
-                              </View>
-                            </ImageBackground>
+                    <ScrollView style={styles.containerProfile}>
+                      <ImageBackground source={IMAGE_01} style={styles.photo}>
+                        <View style={styles.top}></View>
+                      </ImageBackground>
 
-                            <ProfileItem
+                      <ProfileItem
                         matches={IMAGE_01}
                         name={"Nabil Fayak"}
                         age={"18"}
@@ -76,31 +84,42 @@ const Matches = () => {
                         phone={"555-555-5555"}
                         email={"nabilfayak@gmail.com"}
                         linkedin={"https://www.Linkedin.com/in/NabilFayak"}
-                        github={"https://Github.com/NabilFayak"} type={""}                            />
+                        github={"https://Github.com/NabilFayak"}
+                        type={""}
+                      />
 
-                            <View style={styles.actionsProfile}>
-                              {/* <TouchableOpacity style={styles.circledButton}>
+                      <View style={styles.actionsProfile}>
+                        {/* <TouchableOpacity style={styles.circledButton}>
                                 <Icon name="ellipsis-horizontal" size={20} color={WHITE} />
                               </TouchableOpacity> */}
 
-                              <TouchableOpacity style={styles.roundedButton} onPress={() => Linking.openURL('mailto:nabilfayak@gmail.com')}>
-                                <Text style={styles.textButton}>Send Email </Text>
-                                <Icon name="send" size={20} color={WHITE} />
-                              </TouchableOpacity>
-                            </View>
-                          </ScrollView>
-                        </ImageBackground>
-                        <TouchableOpacity style={styles.roundedButton} onPress={() => setModalVisible(!modalVisible)}>
-                            <Icon name="close" size={20} color={WHITE} />
-                            <Text style={styles.textButton}>Close</Text>
+                        <TouchableOpacity
+                          style={styles.roundedButton}
+                          onPress={() =>
+                            Linking.openURL("mailto:nabilfayak@gmail.com")
+                          }
+                        >
+                          <Text style={styles.textButton}>Send Email </Text>
+                          <Icon name="send" size={20} color={WHITE} />
                         </TouchableOpacity>
                       </View>
-                  </Modal>
+                    </ScrollView>
+                  </ImageBackground>
+                  <TouchableOpacity
+                    style={styles.roundedButton}
+                    onPress={() => setModalVisible(!modalVisible)}
+                  >
+                    <Icon name="close" size={20} color={WHITE} />
+                    <Text style={styles.textButton}>Close</Text>
+                  </TouchableOpacity>
+                </View>
+              </Modal>
             </TouchableOpacity>
           )}
         />
       </View>
     </ImageBackground>
-)};
+  );
+};
 
 export default Matches;
