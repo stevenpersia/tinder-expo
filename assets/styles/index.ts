e77757fb23a99501268dd6d9d3e4d36dfea906d7
Haseeb Javed <haseebjaved4652@gmail.com>
2021-09-07 14:53:45 -0400

    styling buttons
    index.tsx

--- assets/styles/index.ts
+++ assets/styles/index.ts
@@ -303,4 +303,46 @@ export default StyleSheet.create({
   iconMenu: {
     alignItems: "center",
   },
+//MODAL
+centeredView: {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  marginTop: 22
+},
+modalView: {
+  margin: 20,
+  backgroundColor: "white",
+  borderRadius: 20,
+  padding: 35,
+  alignItems: "center",
+  shadowColor: "#000",
+  shadowOffset: {
+    width: 0,
+    height: 2
+  },
+  shadowOpacity: 0.25,
+  shadowRadius: 4,
+  elevation: 5
+},
+modalButton: {
+  borderRadius: 20,
+  padding: 10,
+  elevation: 2
+},
+buttonOpen: {
+  backgroundColor: "#F194FF",
+},
+buttonClose: {
+  backgroundColor: "#2196F3",
+},
+textStyle: {
+  color: "white",
+  fontWeight: "bold",
+  textAlign: "center"
+},
+modalText: {
+  marginBottom: 15,
+  textAlign: "center"
+}
 });
